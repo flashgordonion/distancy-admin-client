6c88b8ac5d92a26942b84280665ffb40d6dbf5a4
Arman Bedrosyan <redarman@hotmail.com>
2020-03-24 12:55:39 -0700

    merged

--- src/Pages/RootPage.js
+++ src/Pages/RootPage.js
@@ -2,48 +2,6 @@ import React, {useState} from 'react';
 import { Grid, AppBar, Tabs, Tab} from '@material-ui/core'
 import EnforcerPage from './EnforcerPage';
 import AdminPage from './AdminPage';
-<<<<<<< HEAD
-import CustomerPage from './CustomerPage';
-
-import Footer from '../Components/Footer.js';
-
-const RootPage = () => {
-
-  const [ selectedTab, setSelectedTab ] = useState(2)
-
-  return (
-    <Grid>
-      <AppBar position='absolute'>      
-        <Tabs value={selectedTab} onChange={(event, newValue) => {setSelectedTab(newValue)}}>
-          <Tab label="Enforcer"/>
-          <Tab label="Admin"/>
-          <Tab label="Customer"/>
-        </Tabs>
-      </AppBar>
-      {/* <main> */}
-      <Grid container style={{marginTop: '60px'}} justify="center">
-        { selectedTab === 0 &&
-          <Grid item>
-            <EnforcerPage/>
-          </Grid>
-        }
-        { selectedTab === 1 &&
-          <Grid item>
-            <AdminPage/>
-          </Grid> 
-        }
-        { selectedTab === 2 &&
-          <Grid item>
-            <CustomerPage/>
-          </Grid> 
-        }
-      </Grid>
-      {/* </main> */}
-
-      <Footer/>
-    </Grid>
-  )
-=======
 import LoginPage from './LoginPage';
 
 const RootPage = () => {
@@ -80,7 +38,6 @@ const RootPage = () => {
         {/* </main> */}
       </div>
     )
->>>>>>> dev
 }
 
 export default RootPage

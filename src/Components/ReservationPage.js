6267fc04152e8e3b14038093750309a0a05c251d
Gordon <flashgordonion@gmail.com>
2020-03-25 18:46:37 -0700

    Get Reservation Page working

--- src/Components/ReservationPage.js
+++ src/Components/ReservationPage.js
@@ -1,14 +1,26 @@
-import React, {useState} from 'react';
-import { Grid, Container, Card, CardContent, CardHeader} from '@material-ui/core';
+import React, {useState, useContext, useEffect} from 'react';
+import { Button, Grid, Container, Card, CardContent, CardHeader, MenuItem, Select, InputLabel, TextField} from '@material-ui/core';
 import StoreSummary from './StoreSummary';
-import { KeyboardDateTimePicker } from '@material-ui/pickers';
+import { KeyboardTimePicker, KeyboardDatePicker, KeyboardDateTimePicker } from '@material-ui/pickers';
 import ReservationSlot from './ReservationSlots';
 import moment from 'moment'
+import CustomerStore from '../Contexts/CustomerStore';
 
 
 const ReservationPage = (props) => {
-    
-    const [searchDateTime, setSearchDateTime] = useState(moment())
+    const customerContext = useContext(CustomerStore)
+
+    // const [searchDateTime, setSearchDateTime] = useState(moment())
+    // const [searchRange, setSearchRange] = useState(3)
+    const [searchParams, setSearchParams] = useState({
+        "searchDateTime": moment(),
+        "searchOffset": 3
+    })
+
+    // TODO wait untilauthorized
+    // useEffect(() => {
+    //     customerContext.findSlots({...searchParams})
+    // }, [searchParams])
 
     return (
         <Container maxWidth='md'>
@@ -22,32 +34,60 @@ const ReservationPage = (props) => {
                             title="Find open times"
                         />
                         <CardContent>
-                            <Grid container spacing={1}>
+                            <Grid container direction='column' spacing={1}>
                                 <Grid item>
-                                <KeyboardDateTimePicker
-                                    variant="inline"
-                                    label="Search"
-                                    ampm={true}
-                                    value={searchDateTime}
-                                    onChange={setSearchDateTime}
-                                    // onError={console.log}
-                                    disablePast
-                                    format="YYYY/MM/DD HH:mm"
-                                />  
+                                    <Grid container spacing={1}>
+                                        <Grid item>
+                                            <KeyboardDateTimePicker
+                                                ampm={true}
+                                                label="Search Time"
+                                                value={searchParams.searchDateTime}
+                                                onChange={searchDateTime => setSearchParams({...searchParams, searchDateTime})}
+                                                onError={console.log}
+                                                disablePast
+                                                openTo='hours'
+                                                format="MM/DD h:mm A"
+                                                minutesStep={5}
+                                            />
+                                        </Grid>
+                                        <Grid item>
+                                            
+                                            <TextField
+                                                labelId="hours-label"
+                                                label="Hours"
+                                                select
+                                                value={searchParams.searchOffset}
+                                                onChange={ event => setSearchParams({...searchParams, searchOffset: event.target.value})}
+                                            >
+                                                {[1,2,3,4].map( item => <MenuItem value={item}>{item} Hours</MenuItem>)}
+                                            </TextField>
+                                        </Grid>
+                                        
+                                    </Grid>
+                                </Grid>
+                                <Grid item>
+                                    <Button
+                                        onClick={
+                                            () => customerContext.findSlots(searchParams.searchDateTime, searchParams.searchOffset)
+                                        }
+                                    >
+                                        Search
+                                    </Button>
                                 </Grid>
                             </Grid>
                         </CardContent>
                     </Card>
                 </Grid>
                 {
-                    [1,2,3,4].map( (item) => {
+                    customerContext.openSlots.map( (item, idx) => {
                         return (
-                            <Grid item key={item}><Card><CardContent><ReservationSlot  /></CardContent></Card></Grid>)
+                            <Grid item key={idx}><Card><CardContent><ReservationSlot slot={item} /></CardContent></Card></Grid>)
                     })
                 }
                 
             </Grid>
         </Container>
+        
     )
 }
 
--- src/Contexts/CustomerStore.js
+++ src/Contexts/CustomerStore.js
@@ -1,29 +1,51 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect, useContext} from 'react';
 import axios from 'axios';
-import { ENV_CONFIG } from '../Config/config'
-
+import { ENV_CONFIG } from '../Config/config';
+import AuthorizationContext from './AuthorizationStore';
 
 const Context = React.createContext()
 
 export const CustomerStore = (props) => {
 
-    const [state, setState] = useState({})
+    const authContext = useContext(AuthorizationContext)
+
     const [ticket, setTicket] = useState({})
-    const [searchParams, setSearchParams] = useState({})
-    const [openSlots, getOpenSlots] = useState({})
+    const [openSlots, setOpenSlots] = useState([])
 
     // We want to ensure search is reset on page reload
     useEffect( () => {
 
     }, [])
 
-    const findSlots = async (datetime) => {
-
+    const findSlots = async (searchDateTime, searchOffset) => {
+        try {
+            let resp = await axios.post(
+                `${ENV_CONFIG['BACKEND_URL']}/searchReservations/`,
+                {
+                    store_id: 1,
+                    search_date: searchDateTime,
+                    offset: searchOffset
+                },
+                {
+                    "headers": {'Authorization': `Token ${authContext.token}`}
+                }
+            )
+            console.log(resp.data)
+            setOpenSlots(resp.data['slots'])
+        } catch (error) {
+            // Remove invalid token
+            if (error?.response?.status === 401) {
+                
+            }
+            //TODO - alert to unexpected response
+        } finally {
+        }
     }
     
     return (
         <Context.Provider value = {{
-            ...state,
+            openSlots,
+            findSlots
         }}>
             {props.children}
         </Context.Provider>
